06ae05e227e37676404406f7c9ba3bbc3ec09ee6
Zero0star <3265476988@qq.com>
2026-09-07 23:33:21 +0800

    SS

--- AINEW.lua
+++ AINEW.lua
@@ -1,1 +1,1 @@
-require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("Hi! I'm your game assistant, EggDog!. Do you have anything you want to ask me?",true)
+require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("This is A60, he doesn't have a very good temper.You'll have really good luck in your next few rooms!",true)

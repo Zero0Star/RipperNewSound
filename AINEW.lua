e5cdf7514950d37765cc4faf206979493698761a
Zero0star <3265476988@qq.com>
2026-09-21 20:33:19 +0800

    s

--- AINEW.lua
+++ AINEW.lua
@@ -1,1 +1,30 @@
-require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("You'll get something good in the next room",true)
+local rooms = workspace:WaitForChild("CurrentRooms")
+
+local colors = {
+    Color3.fromRGB(255,0,0),
+    Color3.fromRGB(255,127,0),
+    Color3.fromRGB(255,255,0),
+    Color3.fromRGB(0,255,0),
+    Color3.fromRGB(0,255,255),
+    Color3.fromRGB(0,0,255),
+    Color3.fromRGB(170,0,255)
+}
+
+local i = 1
+
+while true do
+    for _, room in pairs(rooms:GetChildren()) do
+        for _, obj in pairs(room:GetDescendants()) do
+            if obj:IsA("BasePart") then
+                obj.Color = colors[i]
+            end
+        end
+    end
+
+    i += 1
+    if i > #colors then
+        i = 1
+    end
+
+    task.wait(1)
+end

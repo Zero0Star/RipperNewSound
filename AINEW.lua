92b73f6e17bd027f3eb446119021565f5b91cf1b
Zero0star <3265476988@qq.com>
2026-09-21 22:54:45 +0800

    s

--- AINEW.lua
+++ AINEW.lua
@@ -1,10 +1,1 @@
-local DoorsNotify = loadstring(game:HttpGet("https://raw.githubusercontent.com/Guestly-Alt/Scripts/refs/heads/main/AchievementHolder.lua"))()
-
-DoorsNotify({
-    Style = "Task completed!",
-    Title = "Hate Tracker",
-    Description = "He just wants to mess around with you...",
-    Reason = "Survive The HAT RED",
-    Image = "rbxassetid://153700391",
-    Time = 5
-})
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Not-Guestly/Scripts/refs/heads/main/Viridislight-Jug"))()

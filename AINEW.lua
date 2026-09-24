e1d0be88ccffc9c1f494fb39d7dc7109a65dfbe7
Zero0star <3265476988@qq.com>
2026-09-24 21:22:39 +0800

    SS

--- AINEW.lua
+++ AINEW.lua
@@ -1,6 +1,6 @@
 local sound = Instance.new("Sound")
 sound.Name = "Subspace"
-sound.SoundId = "rbxassetid://9128419519"
+sound.SoundId = "rbxassetid://124233495227925"
 sound.Volume = 1
 sound.Parent = workspace
 

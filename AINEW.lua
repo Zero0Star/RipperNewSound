a1e13a409917d3fc7fed0a4c8eab2f7483851a57
Zero0star <3265476988@qq.com>
2026-09-26 23:10:12 +0800

    SS

--- AINEW.lua
+++ AINEW.lua
@@ -1,1 +1,1 @@
-require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("Survive 10 rounds, let me see your guts",true)
+require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("即使此刻。",true)

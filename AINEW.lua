1d6ae5709435d74b1fcc544a7c00e20c00ca83f1
Zero0star <3265476988@qq.com>
2026-09-26 22:36:21 +0800

    SS

--- AINEW.lua
+++ AINEW.lua
@@ -3,18 +3,16 @@ Event:FireServer(
     "DELETE ALL",
     {}
 )
-    local function deleteDirectChildModels()
+ local function deleteDirectChildModelsAndParts()
     local workspace = game:GetService("Workspace")
-    local modelNames = {
-        "A-200", "A60", "Amin-60", "Black-A60", "Deer god","Black Hole Particle effect","DeerGod",
+    local names = { "A-200", "A60", "Amin-60", "Black-A60", "Deer god","Black Hole Particle effect","DeerGod",
         "Frostbite", "@&%^#*$Indescribable God!@$*&^!Q(* ", "LightSpeed",
-        "Rebound", "Ripper", "Following_ENEMY", "Silence","Dread","Muffler","Common Sence","Fluster","Kitty","Broken eyes","Angry Munci","Shadow","LEVEL0","Him","Hunger","WH1T3","Obsession","HimMoving","smiler", "Chainsmoker"
-    }
-    for _, name in ipairs(modelNames) do
-        local model = workspace:FindFirstChild(name)
-        if model and model:IsA("Model") then
-            model:Destroy()
+        "Rebound", "Ripper", "Following_ENEMY", "Silence","Dread","Muffler","Common Sence","Fluster","Kitty","Broken eyes","Angry Munci","Shadow","LEVEL0","Him","Hunger","WH1T3","Obsession","HimMoving","smiler", "Chainsmoker"} -- 保持不变
+    for _, name in ipairs(names) do
+        local child = workspace:FindFirstChild(name)
+        if child and (child:IsA("Model") or child:IsA("Part")) then
+            child:Destroy()
         end
     end
 end
-deleteDirectChildModels()
+deleteDirectChildModelsAndParts()

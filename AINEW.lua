8febf0902dfa93bbbe31b5256a8cbc17cf489701
Zero0star <3265476988@qq.com>
2026-09-26 22:56:51 +0800

    SS

--- AINEW.lua
+++ AINEW.lua
@@ -1,1 +1,1 @@
-require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption(""SparkleAtom"请保持你的忠诚，我稍后赶到。",true)
+require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("SparkleAtom请保持你的忠诚，我稍后赶到。",true)

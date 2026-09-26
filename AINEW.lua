b71a1aaba1cf963dab95558b13594cb522cb7aeb
Zero0star <3265476988@qq.com>
2026-09-26 23:27:00 +0800

    SS

--- AINEW.lua
+++ AINEW.lua
@@ -1,1 +1,1 @@
-require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("即使此刻。",true)
+require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("随后我会继续观察你",true)

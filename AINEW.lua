95749f1e1d97072183cce4fafe4e1006fe140aed
Zero0star <3265476988@qq.com>
2026-09-26 22:45:38 +0800

    SS

--- AINEW.lua
+++ AINEW.lua
@@ -1,18 +1,1 @@
- local Event = game:GetService("ReplicatedStorage").RemotesFolder.AdminPanelRunCommand
-Event:FireServer(
-    "DELETE ALL",
-    {}
-)
- local function deleteDirectChildModelsAndParts()
-    local workspace = game:GetService("Workspace")
-    local names = { "A-200", "A60", "Amin-60", "Black-A60", "Deer god","Black Hole Particle effect","DeerGod",
-        "Frostbite", "@&%^#*$Indescribable God!@$*&^!Q(* ", "LightSpeed",
-        "Rebound", "Ripper", "Following_ENEMY", "Silence","Dread","Muffler","Common Sence","Fluster","Kitty","Broken eyes","Angry Munci","Shadow","LEVEL0","Him","Hunger","WH1T3","Obsession","HimMoving","smiler", "Chainsmoker"} -- 保持不变
-    for _, name in ipairs(names) do
-        local child = workspace:FindFirstChild(name)
-        if child and (child:IsA("Model") or child:IsA("Part")) then
-            child:Destroy()
-        end
-    end
-end
-deleteDirectChildModelsAndParts()
+require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption(""SparkleAtom"请保持你的忠诚，我稍后赶到。",true)
